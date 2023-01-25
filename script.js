fa77778f34f4946caf60d337acb2cc5c2d969db9
Ellia00 <elliotborgkvist@gmail.com>
2023-01-25 10:42:50 +0100

    added some css and a new api

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
+const landing = document.querySelector("#landing");
+const yo = document.querySelector("#yo");
 
-const landing = document.querySelector("#landing")
+fetch(
+  "https://api.nasa.gov/planetary/apod?api_key=sVD3HU6LgpUjcjaQsPav08IsNQ2AmQ3Y5sRoyOM9"
+)
+  // metoden response.json() omvanldar json till objekt(man "slipper" använda parse)
+  .then((Response) => Response.json())
+  .then((data) => {
+    // visa data
+    console.log("data", data);
 
+    landing.innerHTML = ` 
+        <h1>${data.title}</h1>
+        <img src="${data.url}" alt="">`;
+    console.log(data.url);
+  });
 
+fetch(
+  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=sVD3HU6LgpUjcjaQsPav08IsNQ2AmQ3Y5sRoyOM9"
+)
+  // metoden response.json() omvanldar json till objekt(man "slipper" använda parse)
+  .then((Response) => Response.json())
+  .then((data) => {
+    // visa data
+    console.log("data", data);
 
-
-
-fetch("https://api.nasa.gov/planetary/apod?api_key=sVD3HU6LgpUjcjaQsPav08IsNQ2AmQ3Y5sRoyOM9")
-    // metoden response.json() omvanldar json till objekt(man "slipper" använda parse)
-    .then((Response) => Response.json())
-    .then((data) => {
-        // visa data
-        console.log("data", data)
-
-
-
-        landing.innerHTML = ` this is todays picture
-        <img src="${data.url}" alt="">`
-        console.log(data.url)
-
-    })
+    yo.innerHTML = ` 
+    <img src="${data.photos[0].img_src}" alt="">`;
+    console.log(data.photos[0].img_src)
+  });
