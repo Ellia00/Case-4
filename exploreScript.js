a90a8e1a064873369b1eece6fcc5c82f19bbe65a
Ellia00 <elliotborgkvist@gmail.com>
2023-02-02 14:00:53 +0100

    can finally get the href links to console, thx henry!

--- exploreScript.js
+++ exploreScript.js
@@ -1,6 +1,7 @@
 
 
 
+const yo = document.querySelector("#yo");
 
 
 // fetch("https://images-api.nasa.gov/search?q=sputnik")
@@ -8,30 +9,33 @@ fetch("sputnikAPI.json")
     .then((Response) => Response.json())
     .then((data) => {
         console.log(data)
+        
         let { items } = data.collection
-        console.log(data.collection.items[2].links[0].href)
+
+        // console.log(data.collection.items[2].links[0].href)
+        
+
 
         items.forEach(element => {
-            // console.log(element.links)
-
+            console.log(element.links?.[0].href)
+                // kan också loopas med en if sats som kollar om hur lång links är och om den har en nolla i, för alla har inte det, därför den crahsar utan ?
 
 
         });
 
-        let a = [data.collection.items]
-        console.log(a)
+    
 
-        for (let i = 0; i < a.length; i++) {
-            const element = a[i];
-            console.log(element)
+        // for (let i = 0; i < a.length; i++) {
+        //     const element = a[i];
+        //     console.log(element)
 
-            for (let x = 0; x < element.length; x++) {
-                const link = element[x];
-                console.log(link.links)
+        //     for (let x = 0; x < element.length; x++) {
+        //         const link = element[x];
+        //         console.log(link.links)
 
-            }
+        //     }
 
-        }
+        // }
 
 
         // a.forEach(element => {
@@ -48,7 +52,7 @@ fetch("sputnikAPI.json")
 
         yo.innerHTML =
 
-            ` <img src="${data.collection.items[1].links[0].href}" alt=""> `;
+            ` <img src="${data.collection.items[4].links[0].href}" alt=""> `;
 
 
         // console.log("yeehaaw", data.collection.items[1].links[0].href);
