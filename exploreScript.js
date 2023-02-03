d36530984b53132e8967ce46b96d12a71b2e374c
Ellia00 <elliotborgkvist@gmail.com>
2023-02-03 18:02:15 +0100

    added search function and linked logo to homepage

--- exploreScript.js
+++ exploreScript.js
@@ -1,70 +1,38 @@
+const landing = document.querySelector("#landing");
+const searchFormEl = document.querySelector("#searchForm");
 
 
 
-const yo = document.querySelector("#yo");
+searchFormEl.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    let searchString = e.target.search.value
+    console.log(searchString, "this is target")
 
 
-// fetch("https://images-api.nasa.gov/search?q=sputnik")
-fetch("sputnikAPI.json")
-    .then((Response) => Response.json())
-    .then((data) => {
-        console.log(data)
-        
-        let { items } = data.collection
+    // fetch(`https://images-api.nasa.gov/search?q=${searchString}`)
 
-        // console.log(data.collection.items[2].links[0].href)
-        
+        fetch("sputnikAPI.json")
+        .then((Response) => Response.json())
+        .then((data) => {
+            console.log(data)
 
+            let { items } = data.collection
 
-        items.forEach(element => {
-            console.log(element.links?.[0].href)
+            // console.log(data.collection.items[2].links[0].href)
+
+            items.forEach(element => {
+                console.log(element.links?.[0].href)
                 // kan också loopas med en if sats som kollar om hur lång links är och om den har en nolla i, för alla har inte det, därför den crahsar utan ?
+                landing.innerHTML += `
 
-
+        <div class="card">
+            <div class="cardTitle">${element.data[0].title}</div>
+            <img src="${element.links?.[0].href}" class ="cardImg"> 
+            <div class="cardText">"${element.data[0].description}"</div>
+          </div>         
+            `;
+            });
         });
-
-    
-
-        // for (let i = 0; i < a.length; i++) {
-        //     const element = a[i];
-        //     console.log(element)
-
-        //     for (let x = 0; x < element.length; x++) {
-        //         const link = element[x];
-        //         console.log(link.links)
-
-        //     }
-
-        // }
-
-
-        // a.forEach(element => {
-        //     console.log(element.links[0], "hej hej")
-        //     return b = element.links
-        //     // loopa igen
-
-        // });
-
-        // b.forEach(element => {
-        //     console.log(element.href)
-        // });
-
-
-        yo.innerHTML =
-
-            ` <img src="${data.collection.items[4].links[0].href}" alt=""> `;
-
-
-        // console.log("yeehaaw", data.collection.items[1].links[0].href);
-
-
-
-        // for (const property in data.collection.items) {
-        //     if (Object.hasOwnProperty.call(data.collection.items, property)) {
-        //         // console.log(data)
-        //         // console.log(data.collection.items[property])
-        //         // console.log(data.collection.items[property].links)
-        //     }
-        // }
-
-    });
+})
