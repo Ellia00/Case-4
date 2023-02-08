8906516c021246849965714eb4b6f3f11d38e17b
Ellia00 <elliotborgkvist@gmail.com>
2023-02-08 23:04:20 +0100

    sorting finally works thank god

--- exploreScript.js
+++ exploreScript.js
@@ -10,6 +10,8 @@ searchFormEl.addEventListener("submit", (e) => {
     let searchString = e.target.search.value
     console.log(searchString, "this is target")
 
+    landing.innerHTML = ""
+
 
     // fetch(`https://images-api.nasa.gov/search?q=${searchString}`)
 
@@ -20,15 +22,40 @@ searchFormEl.addEventListener("submit", (e) => {
 
             let { items } = data.collection
 
-            // console.log(data.collection.items[2].links[0].href)
+            // if (data.collection.items.length === 0) {
+            //     landing.innerHTML =
+            //         `
+            //     <h1 id="noData"> No Data Found</h1>
+            //     `
+            // }
 
-            items.forEach(element => {
+            let filteredArray =[]
+
+            if (document.getElementById("b2000").checked) {
+                console.log("b2000 is checked")
+                filteredArray = data.collection.items.filter(item => new Date(item.data[0].date_created) < new Date("2000-01-01T00:00:00Z"))
+
+            }
+
+            else if (document.getElementById("a2000").checked) {
+                console.log("a2000 is checked")
+                filteredArray = data.collection.items.filter(item => new Date(item.data[0].date_created) > new Date("2000-01-01T00:00:00Z"))
+            }
+            else {
+                console.log("none is checked")
+                filteredArray= items
+            }
+            console.log(filteredArray, "this shit filtered")
+
+
+
+            filteredArray.forEach(element => {
+                console.log(element.data[0].date_created)
+
 
                 if (element.hasOwnProperty("links")) {
 
                     console.log(element.links?.[0].href)
-
-
                     landing.innerHTML += `
 
                     <div class="card">
@@ -40,11 +67,15 @@ searchFormEl.addEventListener("submit", (e) => {
                       </div>         
                       </div>         
                         `;
-
                 }
 
-                // kan också loopas med en if sats som kollar om hur lång links är och om den har en nolla i, för alla har inte det, därför den crahsar utan ?
-
             });
-        });
+        })
+        .catch(err => {
+
+            landing.innerHTML =
+                `
+                <h1 id="noData"> No Data Found</h1>
+                `
+        })
 })
--- script.js
+++ script.js
@@ -8,30 +8,11 @@ fetch(
   "apod.json"
 )
 
-  // metoden response.json() omvanldar json till objekt(man "slipper" använda parse)
   .then((Response) => Response.json())
   .then((data) => {
-    // visa data
-    console.log("data", data);
 
     landing.innerHTML += ` 
         <h1 id="title">${data.title}</h1>
         <img src="${data.url}" alt="">
         <p id ="explanation">${data.explanation}</p>`;
-    console.log(data.url);
   });
-
-
-// fetch(
-//   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=sVD3HU6LgpUjcjaQsPav08IsNQ2AmQ3Y5sRoyOM9"
-// )
-//   // metoden response.json() omvanldar json till objekt(man "slipper" använda parse)
-//   .then((Response) => Response.json())
-//   .then((data) => {
-//     // visa data
-//     console.log("data", data);
-
-//     yo.innerHTML = `
-//     <img src="${data.photos[0].img_src}" alt="">`;
-//     console.log(data.photos[0].img_src)
-//   });
