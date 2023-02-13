bf3b1445b2705fa300f9af1e72c8f61150b27059
Ellia00 <elliotborgkvist@gmail.com>
2023-02-13 15:46:17 +0100

    made functions, made "date" prettier,fixed no data

--- exploreScript.js
+++ exploreScript.js
@@ -1,15 +1,34 @@
 const landing = document.querySelector("#landing");
 const searchFormEl = document.querySelector("#searchForm");
 
+function render(element) {
 
+    landing.innerHTML += `
+                    <div class="card">
+            
+                    <div class="border">
+                        <div class="cardTitle">${element.data[0].title}</div>
+                        <img src="${element.links?.[0].href}" class ="cardImg"> 
+                        <div class="cardText">"${element.data[0].description}"</div>
+                        <div class="cardText">"Date: ${element.data[0].date_created.slice(0, 10)}"</div>
+
+                      </div>         
+                      </div>         
+                        `;
+}
+
+function renderNoData() {
+    landing.innerHTML = `<h1 id="noData"> No Data Found</h1> `
+}
 
 searchFormEl.addEventListener("submit", (e) => {
 
     e.preventDefault()
 
+    landing.innerHTML = ""
+
     let searchString = e.target.search.value
 
-    landing.innerHTML = ""
 
     fetch(`https://images-api.nasa.gov/search?q=${searchString}`)
 
@@ -20,46 +39,34 @@ searchFormEl.addEventListener("submit", (e) => {
 
             let { items } = data.collection
 
+
+            if (items.length === 0) {
+                renderNoData()
+            }
+
+
             let filteredArray = []
 
             if (document.getElementById("b2000").checked) {
-                console.log("b2000 is checked")
                 filteredArray = data.collection.items.filter(item => new Date(item.data[0].date_created) < new Date("2000-01-01T00:00:00Z"))
             }
 
             else if (document.getElementById("a2000").checked) {
-                console.log("a2000 is checked")
                 filteredArray = data.collection.items.filter(item => new Date(item.data[0].date_created) > new Date("2000-01-01T00:00:00Z"))
             }
             else {
-                console.log("none is checked")
                 filteredArray = items
             }
 
             filteredArray.forEach(element => {
 
                 if (element.hasOwnProperty("links")) {
-
-                    landing.innerHTML += `
-                    <div class="card">
-            
-                    <div class="border">
-                        <div class="cardTitle">${element.data[0].title}</div>
-                        <img src="${element.links?.[0].href}" class ="cardImg"> 
-                        <div class="cardText">"${element.data[0].description}"</div>
-                        <div class="cardText">"Date ${element.data[0].date_created}"</div>
-
-                      </div>         
-                      </div>         
-                        `;
+                    render(element)
                 }
 
             });
         })
         .catch(err => {
-            landing.innerHTML =
-                `
-                <h1 id="noData"> No Data Found</h1>
-                `
+            renderNoData()
         })
 })
